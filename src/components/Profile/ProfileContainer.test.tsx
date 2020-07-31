1adbf258d369bfa584b4e88d4e366dedd2fd5b00
Eduard Shvetsov <eduard.shvetsov@anvileight.com>
2020-07-31 10:58:52 +0300

    Add test for connected component

--- src/components/Profile/ProfileContainer.test.tsx
+++ src/components/Profile/ProfileContainer.test.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import {render, RenderResult} from "@testing-library/react";
 import ProfileContainer from "./ProfileContainer";
-import {MockStore} from "redux-mock-store";
 import {Provider} from "react-redux";
 import {mockStore} from "../../utils/test/mock-store";
 import {BrowserRouter} from "react-router-dom";
 
 describe('Profile container', () => {
-    let store: MockStore;
     let component: RenderResult;
 
     beforeEach(() => {
@@ -18,11 +16,9 @@ describe('Profile container', () => {
                     <ProfileContainer/>
                 </Provider>
             </BrowserRouter>
-        )
+        );
     });
 
     test('should render profile', () => {
-
-        console.log(component);
     });
 });
